74814b67a45ca2011916cc86c1c2f311834c4411
motadou <motadou@126.com>
2017-03-05 21:11:15 +0800

    initialize

---
+++ source/main.cpp
@@ -0,0 +1,18 @@
+#include <QtGui>
+#include "src/main_widget/main_widget.h"
+//#include "main_widget.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+
+
+//	QFont font("Î¢ÈíÑÅºÚ", 9);
+//	app.setFont(font);
+
+    MainWidget main_widget;
+    main_widget.showWidget();
+
+    return app.exec();
+}
